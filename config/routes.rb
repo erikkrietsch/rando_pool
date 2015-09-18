b62da91ccc1ccc4cac4d8e351f9aa375f51a29c4
Jon Allured <jon.allured@gmail.com>
2015-09-18 13:24:32 -0500

    Add a proper homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get '/seasons', to: 'seasons#index', as: :seasons
   get '/seasons/:season_name', to: 'seasons#show', as: :season
   get '/seasons/:season_name/weeks/:week_number', to: 'weeks#show', as: :season_week
-  root to: 'seasons#index'
+  root to: 'pages#home'
 end
