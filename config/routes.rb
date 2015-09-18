8c16b7b5b757b4b82b2b960c26591f36b544f9bf
Jon Allured <jon.allured@gmail.com>
2015-09-18 13:17:46 -0500

    Create season index path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get '/seasons', to: 'seasons#index', as: :seasons
   get '/seasons/:season_name', to: 'seasons#show', as: :season
   get '/seasons/:season_name/weeks/:week_number', to: 'weeks#show', as: :season_week
-  root to: 'seasons#index', as: :seasons
+  root to: 'seasons#index'
 end
