2d2870412fbf9d11abd304d1f4fddfe21d58aebc
Jon Allured <jon.allured@gmail.com>
2015-10-09 14:58:28 -0500

    Remove weeks without picks

--- app/controllers/api/current_season/rando_picks_controller.rb
+++ app/controllers/api/current_season/rando_picks_controller.rb
@@ -1,5 +1,5 @@
 class Api::CurrentSeason::RandoPicksController < ApplicationController
   expose(:season) { Season.order(:id).last }
-  expose(:random_picks) { season.weeks.map &:random_pick }
+  expose(:random_picks) { season.weeks.map(&:random_pick).compact }
   expose(:current_picks) { random_picks.map &CurrentPick.method(:new) }
 end
